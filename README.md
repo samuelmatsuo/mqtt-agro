013743253d5834ba12a61b9f6285f246a3dc8455
Samuel Bampi Matsuo <samuel.matsuo@ixcsoft.com.br>
2025-02-24 17:17:36 -0300

    update: create validation in qos

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mqtt-agro
+O que acontece no QoS 2 no HiveMQ?
+O publisher envia a mensagem com QoS 2 para o broker.
+O broker confirma o recebimento enviando PUBREC para o publisher.
+O publisher responde com PUBREL.
+O broker confirma a entrega ao subscriber enviando PUBCOMP.
+O subscriber recebe a mensagem.
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,50 @@
-import { IPublishPacket } from "mqtt";
+import { IPubcompPacket, IPublishPacket } from "mqtt";
 import { connectionBroker } from "./connections/mqtt";
 
 const client = connectionBroker();
+client.subscribe("#", { qos: 2 });
 
-client.subscribe("#", { qos: 1 });
 client.on(
   "message",
   (topic: string, payload: Buffer, packet: IPublishPacket) => {
-    if (topic === "iot.rain") {
-      console.log(payload.toString());
-    }
-    if (topic === "iot.temperature") {
-      console.log(payload.toString());
-    }
-    if (topic === "iot.humidity") {
-      console.log(payload.toString());
-    }
-    if (topic === "iot.gas") {
-      console.log(payload.toString());
-    }
-    if (topic === "iot.error") {
-      console.log(payload.toString());
-    }
-    if (topic === "iot.off") {
-      console.log(payload.toString());
+    switch (topic) {
+      case "iot.rain":
+        console.log(
+          `Mensagem recebida: Tópico -> ${topic} | QoS -> ${
+            packet.qos
+          } | Payload -> ${payload.toString()}`
+        );
+        break;
+      case "iot.temperature":
+        console.log(
+          `B Mensagem recebida: Tópico -> ${topic} | QoS -> ${
+            packet.qos
+          } | Payload -> ${payload.toString()}`
+        );
+        break;
+      case "iot.humidity":
+        console.log(
+          `Mensagem recebida: Tópico -> ${topic} | QoS -> ${
+            packet.qos
+          } | Payload -> ${payload.toString()}`
+        );
+        break;
+      case "iot.gas":
+        console.log(
+          `Mensagem recebida: Tópico -> ${topic} | QoS -> ${
+            packet.qos
+          } | Payload -> ${payload.toString()}`
+        );
+        break;
+      case "iot.error":
+        console.log(
+          `Mensagem recebida: Tópico -> ${topic} | QoS -> ${
+            packet.qos
+          } | Payload -> ${payload.toString()}`
+        );
+        break;
+      case "iot.off":
+        break;
     }
   }
 );
